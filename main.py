03b03f69cdd2fd084e1e29d25fb5d8818c560979
rafael.novais <rafael.nono@gmail.com>
2023-09-13 20:22:05 +0100

    Class 1 car code NSEW

--- main.py
+++ main.py
@@ -5,18 +5,45 @@ def snooker(n):
         t
     print(t)
 
+
 snooker(12)
 
 
-def mySum(x,y):
-    return x+y
+def mySum(x, y):
+    return x + y
 
 
-def mySub(x,y):
-    return x-y
+def mySub(x, y):
+    return x - y
 
 
+print(mySum(2, 7))
+print(mySub(8, 10))
+
+import math
 
 
-print(mySum(2,7))
-print(mySub(8,10))
+def simulate(prog, xsize, zsize):
+    xtotal = 0
+    ytotal = 0
+
+    for i in prog:
+        if i == "N":
+            xtotal += 1
+        elif i == "S":
+            xtotal -= 1
+        elif i == "E":
+            ytotal -= 1
+        elif i == "W":
+            ytotal += 1
+        print(i)
+        print(xtotal)
+        print(ytotal)
+
+    x = xsize + xtotal
+    y = zsize + ytotal
+    print(x)
+    print(y)
+
+
+simulate("NNNSSEEWW", 100, 100)
